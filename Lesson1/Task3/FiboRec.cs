5b9525b2cbd34361dc6744751cebd83192ab697b
grimashevich <k.grimashevich@gmail.com>
2021-09-30 23:33:45 +0900

    Task 3

---
+++ Lesson1/Task3/FiboRec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task3
+{
+    public class FiboRec
+    {
+        public int Calculate(int x)
+        {
+            if (x < 0)  throw new ArgumentException("Аргумент должен быть >= 1");
+            if (x == 0) return 0;
+            if (x == 1) return 1;
+            return Calculate(x - 1) + Calculate(x - 2);
+        }
+    }
+}
---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 10;
+            var fibo1 = new FiboRec();
+            var fibo2 = new FiboLoop();
+            Console.WriteLine($"Число фибоначчи для f({n}) = {fibo1.Calculate(n)} (рекурсия)");
+            Console.WriteLine($"Число фибоначчи для f({n}) = {fibo2.Calculate(n)} (цикл)");
+        }
+    }
+}
