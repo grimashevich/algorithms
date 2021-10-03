0f06820f909b6fa8955f1fa87e868fa19e191e3b
Konstantin Grimashevich <k.grimashevich@gmail.com>
2021-10-03 23:09:32 +0300

    Task 2

---
+++ Lesson 2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListSearch l1 = new();
+            for (int i = 100; i < 201; i++)
+                l1.Add(i);
+
+            for (int i = 75; i < 226; i += 25)
+                Console.WriteLine($"Ищем {i}, результат: {l1.BinSearch(i)}");
+        }
+    }
+}
