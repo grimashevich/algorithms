65dacab24d6a82c6884bde1c7741bf505e44d560
Konstantin Grimashevich <k.grimashevich@gmail.com>
2021-10-03 21:15:56 +0300

    Task1

---
+++ Lesson 2/Task1/DListMinitester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    public class DListMinitester
+    {
+        public static void DoTest()
+        {
+            var fakeNode = new Node(999);
+            DLinkedList l1 = new DLinkedList();
+            l1.FindNode(5); //Проверка на падение
+            l1.AddNode(100);
+            l1.AddNode(101);
+            l1.AddNodeAfter(l1.First, 102);
+            l1.AddNodeAfter(l1.Last, 103);
+            l1.AddNodeAfter(fakeNode, 888); //Проверка на падение 
+            l1.RemoveNode(-1); //Проверка на падение
+            l1.RemoveNode(5); //Проверка на падение
+            l1.RemoveNode(fakeNode); //Проверка на падение
+            l1.Print();
+            Console.WriteLine($"Количество элементов: {l1.GetCount()}");
+
+            for (int i = 0; i < 5; i++) //Проверка на падение
+            {
+                l1.RemoveNode(l1.First); 
+            }
+        }
+    }
+}
---
+++ Lesson 2/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DListMinitester.DoTest();
+        }
+    }
+}
